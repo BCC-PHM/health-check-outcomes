7dd61556399d58f29334f9e4226db0688d4d90d9
David.Ellis <david.ellis@birmingham.gov.uk>
2026-06-09 18:35:47 +0100

    plotted sex and ethnicity distributions

--- R/demographics.R
+++ R/demographics.R
@@ -36,4 +36,53 @@ age_plt <- ggplot(hc_processed, aes(x = age, fill = sex)) +
 
 age_plt
 
-ggsave("output/demographics/age-hc-2526.png", width = 6, height = 4)
+ggsave("output/demographics/age-hc-2526.png", plot = age_plt,
+       width = 6, height = 4)
+
+# Sex
+
+sex_plt <- hc_processed %>%
+  filter(
+    sex %in% c("Male", "Female")
+    ) %>%
+  ggplot(aes(x = sex, fill = sex)) +
+  geom_bar(color = "black") +
+  theme_bw() +
+  scale_y_continuous(limits = c(0, 20e3), expand = c(0,0)) +
+  labs(
+    fill = "",
+    x = "",
+    y = "",
+    title = "Sex of Birmingham NHS Health Check Attendees (25/26)"
+  ) +
+  theme(
+    legend.position = "none"
+  ) +
+  scale_fill_manual(
+    values = c("#84329B","#FFAD00")
+  )
+sex_plt
+ggsave("output/demographics/sex-hc-2526.png", plot = sex_plt,
+       width = 6, height = 4)
+
+# Ethnicity
+
+eth_plt <- ggplot(hc_processed, aes(x = broad_ethnicity, fill = broad_ethnicity)) +
+  geom_bar(color = "black") +
+  theme_bw() +
+  scale_y_continuous(limits = c(0, 12e3), expand = c(0,0)) +
+  labs(
+    fill = "",
+    x = "",
+    y = "",
+    title = "Broad Ethnicity of Birmingham NHS Health Check Attendees (25/26)"
+  ) +
+  theme(
+    legend.position = "none"
+  ) +
+  scale_fill_manual(
+    values = c("#84329B","#D00070", "#DC582A", "#FFAD00", "#00A9E0", "#75BC22")
+  )
+eth_plt
+ggsave("output/demographics/ethnicity-hc-2526.png", plot = eth_plt,
+       width = 6, height = 4)
